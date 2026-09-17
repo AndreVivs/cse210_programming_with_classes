49e3ca34676599e282dafe599fe5baec5ab33eb4
Andrea Ramos Vivas <andrevivs87@gmail.com>
2026-09-17 10:13:57 -0600

    Exercise 4: Lists and Generics

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (number != 0)
+        {            
+            Console.WriteLine("Enter number (0 to finish):");
+            string userNumber = Console.ReadLine();
+            number = int.Parse(userNumber);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            else if (string.IsNullOrEmpty(userNumber))
+            {
+                Console.WriteLine("Please enter a number or 0 to finish:");
+            }
+            else
+            {
+                Console.WriteLine($"The sum is: {numbers.Sum()}");
+                Console.WriteLine($"The average is: {numbers.Average()}");
+                Console.WriteLine($"The largest number is: {numbers.Max()}");
+                Console.WriteLine($"The smallest positive number is: {numbers.Where(n => n > 0).DefaultIfEmpty().Min()}");
+                Console.WriteLine($"The sorted list is: {string.Join(", ", numbers.OrderBy(n => n))}");
+            }
+        }
     }
 }
