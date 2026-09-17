adfb9375b8d0f59908dab326ce98a1008eadc57f
Andrea Ramos Vivas <andrevivs87@gmail.com>
2026-09-17 09:20:13 -0600

    Exercise 3: Loops with stretch

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumberInt = randomGenerator.Next(1,101);
+
+        int guessNumberInt;
+        int guessCountInt = 0;
+
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            guessNumberInt = int.Parse(Console.ReadLine());
+            guessCountInt++;
+            
+            if (guessNumberInt < magicNumberInt)
+            {
+                Console.WriteLine("Higher");  
+            }
+            else if (guessNumberInt > magicNumberInt)
+            {
+                Console.WriteLine("Lower"); 
+            }
+            else
+            {
+                Console.WriteLine("You guess it!"); 
+                Console.WriteLine($"It took you {guessCountInt} guesses.");
+            }
+        } while (guessNumberInt != magicNumberInt);
     }
 }
